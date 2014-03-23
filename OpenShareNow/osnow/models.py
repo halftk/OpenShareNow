a4760f1be86a196362ae8c23a3567fdc8c17f787
Roger Orellana <roc1@alumnes.udl.cat>
2014-03-23 05:13:59 +0100

    Models enllaçats

--- OpenShareNow/osnow/models.py
+++ OpenShareNow/osnow/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-	nom = models.TextField(max_length=100)
-	descripcio = models.TextField(max_length=1000)
-	link= models.TextField(max_length=500)
-	dataCreacio= models.DateTimeField()
-	dataModificacio= models.DateTimeField()
-	usuari= models.IntegerField()
-	seccio = models.IntegerField()
-	valoracio= models.IntegerField()
-
 class Seccio(models.Model):
 	nom = models.TextField(max_length=100)
 	descripcio = models.TextField(max_length=500)
@@ -18,10 +8,6 @@ class Seccio(models.Model):
 class Categoria(models.Model):
 	nom = models.TextField(max_length=100)
 	descripcio = models.TextField(max_length=500)
-
-class Categoria_aux(models.Model):
-	categoria = models.IntegerField()
-	item = models.IntegerField()
 
 class Usuari(models.Model):
 	login = models.TextField(max_length=15)
@@ -32,38 +18,53 @@ class Usuari(models.Model):
 	dataAlta= models.DateTimeField()
 	dataNeixement= models.DateTimeField()
 
+class Item(models.Model):
+	nom = models.TextField(max_length=100)
+	descripcio = models.TextField(max_length=1000)
+	link= models.TextField(max_length=500)
+	dataCreacio= models.DateTimeField()
+	dataModificacio= models.DateTimeField()
+	usuari= models.ForeignKey(Usuari)
+	seccio = models.ForeignKey(Seccio)
+	valoracio= models.IntegerField()
+
 class Comentari(models.Model):
 	text = models.TextField(max_length=100)
 	dataCreacio = models.DateTimeField()
 	dataModificacio = models.DateTimeField()
-	usuari = models.IntegerField()
-	item = models.IntegerField()
-
-class Comentaris_aux(models.Model):
-	comentari= models.IntegerField()
-	resposta= models.IntegerField()
+	usuari = models.ForeignKey(Usuari)
+	item = models.ForeignKey(Item)
 
 class Idioma(models.Model):
 	nom = models.TextField(max_length=30)
 	imatge = models.TextField(max_length=30)
 
-class Idioma_aux(models.Model):
-	item = models.IntegerField()
-	idioma = models.IntegerField()
 
 class Tag(models.Model):
 	nom = models.TextField(max_length=100)
 
-class Tags_aux(models.Model):
-	item = models.IntegerField()
-	tag = models.IntegerField()
+class Categoria_aux(models.Model):
+	categoria = models.ForeignKey(Categoria)
+	item = models.ForeignKey(Item)
 
-class Valoracio(models.Model):
-	item = models.IntegerField()
-	usuari = models.IntegerField()
-	nota = models.IntegerField(max_length=2)
+class Tags_aux(models.Model):
+	item = models.ForeignKey(Item)
+	tag = models.ForeignKey(Tag)
+
+class Idioma_aux(models.Model):
+	item = models.ForeignKey(Item)
+	idioma = models.ForeignKey(Idioma)
+
+class Comentaris_aux(models.Model):
+	comentari= models.ForeignKey(Comentari,related_name="comentari_principal")
+	resposta= models.ForeignKey(Comentari,related_name="comentari_secundari")
 
 class Seccio_aux(models.Model):
-	seccio = models.IntegerField()
-	subseccio = models.IntegerField()
+	seccio = models.ForeignKey(Seccio,related_name="seccio_principal")
+	subseccio = models.ForeignKey(Seccio,related_name="seccio_secundaria")
+
+class Valoracio(models.Model):
+	item = models.ForeignKey(Item,related_name="item_valorat")
+	usuari = models.ForeignKey(Usuari)
+	nota = models.IntegerField(max_length=2)
 
